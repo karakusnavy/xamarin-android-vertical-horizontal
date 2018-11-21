b90159b33e5662d18cfb4b0775c09161aaebffcc
Samed Karakus <karakusnavy@gmail.com>
2018-11-21 22:24:04 +0300

    updated

---
+++ DedektifHikayeOyunu/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace DedektifHikayeOyunu
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);            
+            SetContentView(Resource.Layout.activity_main);
+            
+        }
+    }
+}
